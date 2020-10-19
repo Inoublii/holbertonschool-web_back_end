7a9d730fd307bce23643dc169f4bb9c805e2c6eb
inoublii <inoublii@gmail.com>
2020-10-19 22:05:14 +0100

    advanced 1

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+safe_first_element
+"""
+from typing import Union, Any, Sequence, List
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
