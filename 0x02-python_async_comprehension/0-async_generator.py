1ef17209f6230d4535611ccea39bf292739c19dc
inoublii <inoublii@gmail.com>
2020-10-21 19:53:56 +0100

    generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" a async generator """
+import asyncio
+from typing import Generator
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ a async generator """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
