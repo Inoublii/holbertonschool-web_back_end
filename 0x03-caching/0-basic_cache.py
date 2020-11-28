1612178d6569225ee34b6b7afd2dab81ce56f0cc
inoublii <inoublii@gmail.com>
2020-11-28 15:48:19 +0000

    caching

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" FIFO Cache """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache Class """
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        """
+        if key and item :
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value of key in self.cache_data
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
