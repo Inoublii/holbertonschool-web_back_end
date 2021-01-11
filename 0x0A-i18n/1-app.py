3ea9808ad8d3ce88cdfa35ac24ce06ec1796acec
inoublii <inoublii@gmail.com>
2021-01-11 20:12:32 +0000

    basic flask

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -5,7 +5,7 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 
-class config(object):
+class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
