982c7b44b7f999f8993358096fca790a1940be42
inoublii <inoublii@gmail.com>
2021-01-11 20:10:58 +0000

    basic flask

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello():
+    """ render html file """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
