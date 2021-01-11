646564d14e93f85644a9618862c212b16133170b
inoublii <inoublii@gmail.com>
2021-01-11 23:01:11 +0000

    added users arg'

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -54,8 +54,7 @@ def get_locale():
     x = request.args.get('locale')
     if x in app.config['LANGUAGES']:
         return x
-    if (g.get('user') and g.user.get("locale", None)
-    and g.user["locale"] in app.config['LANGUAGES']):
+    if (g.get('user') and g.user["locale"] in app.config['LANGUAGES']):
         return g.user["locale"]
     return(request.accept_languages.best_match(app.config['LANGUAGES']))
 
