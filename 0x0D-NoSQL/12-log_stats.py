b5c5e3ee6d294fd7441535f175793b2e16c94d33
inoublii <inoublii@gmail.com>
2021-01-19 02:40:49 +0000

    added function

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -5,22 +5,27 @@ Python script that provides some stats about Nginx logs stored in MongoDB.
 """
 
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def getlogs():
+    """ Provides some stats about Nginx logs stored in MongoDB. """
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx = client.logs.nginx
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
 
-print(nginx.count_documents({})+" logs")
+    print(nginx.count_documents({})+" logs")
 
-print("Methods:")
+    print("Methods:")
 
-for method in methods:
-    print(
-        "\tmethod {}: {}".format(
-            method, nginx.count_documents({'method': method})
+    for method in methods:
+        print(
+            "\tmethod {}: {}".format(
+                method, nginx.count_documents({'method': method})
+            )
         )
-    )
 
-print(
-    nginx.count_documents({'method': 'GET', 'path': '/status'})+" status check"
-    )
+    print(
+        nginx.count_documents(
+            {'method': 'GET', 'path': '/status'})+" status check"
+        )
+if __name__ == "__main__":
+    getlogs()
