70f58e0bf689033002899288c75a8883fc0372c6
inoublii <inoublii@gmail.com>
2021-01-25 20:02:47 +0000

    Log stats

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 """
-Python script that provides stats about Nginx logs stored in MongoDB.
+Python script that provides stats in MongoDB.
 """
 from pymongo import MongoClient
 
 
-def getlogs():
-    """ Provides some stats about Nginx logs stored in MongoDB. """
+def getlogs() -> None:
+    """
+    Nginx logs
+    """
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     client = MongoClient('mongodb://127.0.0.1:27017')
+
     nginx = client.logs.nginx
 
     print(nginx.count_documents({}), " logs")
