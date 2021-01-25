24a5067d94a8309cafb3d098e023f1225f12119a
inoublii <inoublii@gmail.com>
2021-01-25 23:34:21 +0000

    pagination

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -9,7 +9,7 @@ from typing import List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """Server class to paginate.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -18,23 +18,25 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
+                read = csv.reader(f)
+                data = [row for row in read]
+            self.__dataset = data[1:]
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
+
             dataset = self.dataset()
+
             truncated_dataset = dataset[:1000]
+
             self.__indexed_dataset = {
+
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
@@ -44,8 +46,8 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """The method should return a dictionary"""
-        assert (isinstance(index, int)
-                and index in range(len(self.__indexed_dataset)))
+        assert (isinstance(
+            index, int)and index in range(len(self.__indexed_dataset)))
         data = []
         diff = 0
         row = index
@@ -54,6 +56,7 @@ class Server:
                 data.append(self.__indexed_dataset[row])
                 row += 1
                 diff += 1
+
             else:
                 row += 1
         if row < len(self.__indexed_dataset):
@@ -62,4 +65,3 @@ class Server:
             next = None
         return {'index': index, 'next_index': next,
                 'page_size': len(data), 'data': data}
-
