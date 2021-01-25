a7e38231b138cb5b01b5c52a59b8c2de5aa6c9a0
inoublii <inoublii@gmail.com>
2021-01-25 23:49:35 +0000

    pagination

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
             self.__dataset = data[1:]
         return self.__dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) :
+    def get_hyper_index(self, index: int = None, page_size: int = 10):
             pass
 
     def get_hyper_index(self, index: int = None, page_size: int = 10):
