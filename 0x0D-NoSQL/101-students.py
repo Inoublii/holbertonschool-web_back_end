0016994c1904669e9c966a794f2bad5543783884
inoublii <inoublii@gmail.com>
2021-01-26 21:00:04 +0000

    average scrore

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -2,10 +2,13 @@
 """
 Python script that provides some stats about Nginx logs stored in MongoDB.
 """
+
+
 def top_students(mongo_collection):
     """ Returns all the students sorted by average score. """
-    avg =  mongo_collection.aggregate([
-        {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
-        {"$sort": {"averageScore": -1}}
+    avg = mongo_collection.aggregate([
+        {"$project": {"name": "$name", "averageScore":
+         {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": - 1}}
     ])
     return avg
