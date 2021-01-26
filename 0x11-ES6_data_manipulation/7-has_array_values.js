be5da96e1b5358a5e930c43e1565c9b6b98430dc
inoublii <inoublii@gmail.com>
2021-01-26 01:23:54 +0000

    data JS

--- 0x11-ES6_data_manipulation/7-has_array_values.js
+++ 0x11-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
-export default function setFromArray(array) {
-  return new Set(array);
+export default function hasValuesFromArray(set, Arrays) {
+  for (let i = 0; i < Arrays.length; i += 1) {
+    if (!set.has(Arrays[i])) {
+      return false;
+    }
+  }
+  return true;
 }
