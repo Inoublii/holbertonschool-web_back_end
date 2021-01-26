551bc8479049d6f692625e89378be54b5bdb114d
inoublii <inoublii@gmail.com>
2021-01-26 16:12:03 +0000

    constructor class

---
+++ 0x10-ES6_classes/100-evcar.js
@@ -0,0 +1,11 @@
+import Car from './10-car';
+/* eslint-disable */
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+  cloneCar() {
+    return Object.assign(new Car(), this.constructor);
+  }
+}
