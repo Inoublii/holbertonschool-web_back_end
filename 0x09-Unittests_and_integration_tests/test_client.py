21dc603e20a989183ee9453bcdf9856f0894032b
inoublii <inoublii@gmail.com>
2021-01-30 21:10:48 +0000

    task 7

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -59,3 +59,12 @@ class TestGithubOrgClient(TestCase):
             self.assertEqual(r, mocked_public_repos.return_value)
             mocked_public_repos.assert_called_once()
             get_json_mock.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ unit-test for GithubOrgClient.has_license """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
