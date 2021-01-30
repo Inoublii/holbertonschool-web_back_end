796f1ad060594fa45463b094f1134c3a0acad064
inoublii <inoublii@gmail.com>
2021-01-30 19:59:54 +0000

    Task 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -4,30 +4,26 @@
 import unittest
 from parameterized import parameterized
 
-from utils import access_nested_map, get_json
-import requests
-from unittest import mock
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Class for testing Nested Map function """
-    self.assertRaises(KeyError, access_nested_map, nested_map, path)
-
-
-class TestGetJson(unittest.TestCase):
-    '''
-    get_json tests.
-    '''
-
     @parameterized.expand([
-        ('http://example.com', {"payload": True}),
-        ('http://holberton.io', {"payload": False})
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_get_json(self, url, expected_result):
+    def test_access_nested_map(self, map, path, expected_output):
+        """ Test method return output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
         '''
-            Tests if get_json returns expected result.
+            Tests access_nested_map for raised expections.
         '''
-        with mock.patch('utils.requests') as mock_request:
-            mock_request.get.return_value = expected_result
-            x = mock_request.get(url)
-            self.assertEqual(expected_result, x)
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
