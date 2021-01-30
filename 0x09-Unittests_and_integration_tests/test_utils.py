4c85753709fa69a884d376e8e5f3632601cc28a7
inoublii <inoublii@gmail.com>
2021-01-30 19:52:47 +0000

    Task2

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -10,15 +10,7 @@ from unittest import mock
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Class for testing Nested Map function """
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, map, path, expected_output):
-        """ Test method return output """
-        real_output = access_nested_map(map, path)
-        self.assertEqual(real_output, expected_output)
+    self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 class TestGetJson(unittest.TestCase):
     '''
