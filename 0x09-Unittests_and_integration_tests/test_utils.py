b4ec28f4905414dadb638419296fbfe937861a4a
inoublii <inoublii@gmail.com>
2021-01-30 19:42:12 +0000

    Task1

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Test SUITE Unittest module Task """
+
+import unittest
+from parameterized import parameterized
+
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for testing Nested Map function """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected_output):
+        """ Test method return output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
+        
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        '''
+            Tests access_nested_map for raised expections.
+        '''
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
