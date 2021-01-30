748178b8410e6c6de1e21400a5c87d1cd32f5f1d
inoublii <inoublii@gmail.com>
2021-01-30 20:44:42 +0000

    task 4

---
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/env python3
+""" Unittest module """
+
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+import client
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(TestCase):
+    """ Class for testing """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_json):
+        """ returns correct output """
+        gc = GithubOrgClient(org_name)
+        gc.org()
+        mock_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+            )
