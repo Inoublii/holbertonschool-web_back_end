11cfe288bb6dea87d1f48bf96d5335c4e332986b
inoublii <inoublii@gmail.com>
2021-02-01 18:04:24 +0000

    user_credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,5 +45,4 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        credentials = decoded_base64_authorization_header.split(':')
-        return (credentials[0], credentials[1])
+        return tuple(decoded_base64_authorization_header.split(':'))
